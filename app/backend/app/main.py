81e2854e2d3b93484f2b4cb4496db20798c3fafc
Lyndon Lim <80683209+lyndonlim27@users.noreply.github.com>
2025-03-18 13:19:19 +0800

    feat: added startup scripts, simple chat endpoint, readme (#8)
    * feat: added startup scripts, simple chat endpoint, readme
    
    * docs: minor reformat and update docs; update changelog
    
    * fix: fix broken links in docs
    
    * fix: fix broken links in docs; attempt 2
    
    * fix: fix broken links in docs; attempt 3
    
    * fix: fix lint
    
    * fix: fix prettier
    
    ---------
    
    Co-authored-by: Wilsven Leong <wilsven_leong96@hotmail.co.uk>
    Co-authored-by: Roydon Tay <roydontay21@gmail.com>

---
+++ app/backend/app/main.py
@@ -0,0 +1,15 @@
+import uvicorn
+from fastapi import FastAPI
+from routers import chat
+
+
+def create_app():
+    app = FastAPI()
+    app.include_router(chat.router)
+    return app
+
+
+app = create_app()
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
---
+++ scripts/start.ps1
@@ -0,0 +1,15 @@
+if (-not (Get-Command uv -ErrorAction SilentlyContinue)) {
+    Write-Output "uv not installed. Installing..."
+    powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+    $env:Path = "C:\Users\$env:USERNAME\.local\bin;$env:Path"
+}
+
+Write-Output "Creating python virtual environment..."
+uv venv
+. .venv/Scripts/Activate
+
+Write-Output "Syncing dependencies..."
+uv sync
+
+Set-Location app/backend/app
+uvicorn main:app --reload
