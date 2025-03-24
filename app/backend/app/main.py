0af542bc5ab5dca3e2faabd7b4414286381253e1
Wilsven Leong <wilsven_leong96@hotmail.co.uk>
2025-03-25 01:37:25 +0800

    chore: remove stock endpoints as no longer needed

--- app/backend/app/main.py
+++ app/backend/app/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from routers import authentication, booking, chat, record, stock, user, vaccine
+from routers import authentication, booking, chat, record, user, vaccine
 
 
 def create_app():
@@ -10,7 +10,6 @@ def create_app():
     app.include_router(booking.router)
     app.include_router(chat.router)
     app.include_router(record.router)
-    app.include_router(stock.router)
     app.include_router(user.router)
     app.include_router(vaccine.router)
     return app
