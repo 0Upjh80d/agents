5c857b09de5df5067ce58c762d43a9593b180bce
Lyndon Lim <hnghng127@gmail.com>
2025-03-27 12:22:29 +0800

    refactor: combine nearest polyclinic and gp endpoint

--- app/backend/app/main.py
+++ app/backend/app/main.py
@@ -9,10 +9,10 @@ def create_app():
     app.include_router(authentication.router)
     app.include_router(booking.router)
     app.include_router(chat.router)
+    app.include_router(clinic.router)
     app.include_router(record.router)
     app.include_router(user.router)
     app.include_router(vaccine.router)
-    app.include_router(clinic.router)
     return app
 
 
--- app/backend/app/routers/clinic.py
+++ app/backend/app/routers/clinic.py
@@ -2,7 +2,7 @@ from auth.oauth2 import get_current_user
 from fastapi import APIRouter, Depends, HTTPException, status
 from models.database import get_db
 from models.models import Address, Clinic, User
-from schemas.clinic import ClinicResponse
+from schemas.clinic import ClinicResponse, ClinicType
 from sqlalchemy import func
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
@@ -12,13 +12,14 @@ router = APIRouter(prefix="/clinic", tags=["Clinic"])
 
 
 @router.get(
-    "/polyclinic/nearest",
+    "/nearest",
     status_code=status.HTTP_200_OK,
     response_model=list[ClinicResponse],
 )
-async def get_nearest_polyclinic(
-    polyclinic_limit: int = 3,
+async def get_nearest_clinic(
+    clinic_limit: int = 3,
     current_user: User = Depends(get_current_user),
+    clinic_type: ClinicType = ClinicType.POLYCLINIC,
     db: AsyncSession = Depends(get_db),
 ):
 
@@ -36,15 +37,22 @@ async def get_nearest_polyclinic(
         func.radians(Address.latitude - user_latitude), 2
     ) + func.pow(func.radians(Address.longitude - user_longitude), 2)
 
+    print(
+        "type",
+        clinic_type,
+        type(clinic_type),
+        clinic_type.value,
+        type(clinic_type.value),
+    )
     # Query to fetch clinics ordered by distance
     stmt = (
         select(Clinic)
         .join(Clinic.address)
         .options(selectinload(Clinic.address))
         .add_columns(haversine_distance.label("distance"))
-        .where(Clinic.type == "polyclinic")
+        .where(Clinic.type == clinic_type.value)
         .order_by("distance")
-        .limit(polyclinic_limit)
+        .limit(clinic_limit)
     )
 
     result = await db.execute(stmt)
@@ -53,53 +61,7 @@ async def get_nearest_polyclinic(
     if not polyclinics:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="No polyclinics found.",
+            detail=f"No {clinic_type.value} found.",
         )
 
     return polyclinics
-
-
-@router.get(
-    "/gp/nearest", status_code=status.HTTP_200_OK, response_model=list[ClinicResponse]
-)
-async def get_nearest_gp(
-    gp_limit: int = 3,
-    current_user: User = Depends(get_current_user),
-    db: AsyncSession = Depends(get_db),
-):
-
-    user_address_stmt = (
-        select(Address.longitude, Address.latitude)
-        .join(User.address)
-        .where(User.id == current_user.id)
-    )
-
-    result = await db.execute(user_address_stmt)
-    user_longitude, user_latitude = result.first()
-
-    # Calculate the distance using the Haversine formula
-    haversine_distance = func.pow(
-        func.radians(Address.latitude - user_latitude), 2
-    ) + func.pow(func.radians(Address.longitude - user_longitude), 2)
-
-    # Query to fetch clinics ordered by distance
-    stmt = (
-        select(Clinic)
-        .join(Clinic.address)
-        .options(selectinload(Clinic.address))
-        .add_columns(haversine_distance.label("distance"))
-        .where(Clinic.type == "gp")
-        .order_by("distance")
-        .limit(gp_limit)
-    )
-
-    result = await db.execute(stmt)
-    gps = result.scalars().all()
-
-    if not gps:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="No general practitioners found.",
-        )
-
-    return gps
