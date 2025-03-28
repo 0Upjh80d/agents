30913b1f21f4360f20a8626c46858bdeb96c554d
pangyen <e0959104@u.nus.edu>
2025-03-28 15:16:37 +0800

    feat: add agent tools and tool wrappers

---
+++ notebooks/autogen/agent_tools.py
@@ -0,0 +1,393 @@
+import os
+
+import requests
+from dotenv import load_dotenv, set_key
+
+# from agent_tools import (
+#     get_available_booking_slots,
+#     get_booking_by_id,
+#     schedule_vaccination_slot,
+#     cancel_booking,
+#     get_vaccination_history,
+#     get_user_details
+# )
+
+"""
+===================================================
+Development functions
+===================================================
+"""
+
+
+# for development, create a new user and login, save access token to environment as 'AUTH_TOKEN'
+def register_and_login_user(user_data: dict):
+    load_dotenv(override=True)
+    BACKEND_DB_URL = os.getenv("BACKEND_DB_URL")
+
+    reg_url = f"{BACKEND_DB_URL}/signup"
+    login_url = f"{BACKEND_DB_URL}/login"
+
+    response = requests.post(reg_url, json=user_data)
+    if response.status_code == 201:
+        print("User registered successfully!")
+
+        login_url = f"{BACKEND_DB_URL}/login"
+        login_data = {
+            "grant_type": "password",  # Required
+            "username": user_data["email"],  # Email used for login
+            "password": user_data["password"],  # Password used for login
+            "scope": "",  # Optional (empty value sent)
+            "client_id": "",  # Optional (empty value sent)
+            "client_secret": "",  # Optional (empty value sent)
+        }
+        response = requests.post(
+            login_url, data=login_data
+        )  # receive access_token after login
+        if response.status_code == 200:
+            print("User logged in successfully!")
+            access_token = response.json().get("access_token")
+            if access_token:
+                print("Access token received.")
+                os.environ["AUTH_TOKEN"] = access_token
+                set_key(".env", "AUTH_TOKEN", access_token)
+
+                print("Access token saved to environment.")
+            else:
+                print("No access token found in response.")
+
+        else:
+            print("Failed to login user:", response.json())
+    elif response.status_code == 409:
+        print("User already exists.")
+        print("User registered successfully!")
+
+        login_url = f"{BACKEND_DB_URL}/login"
+        login_data = {
+            "grant_type": "password",  # Required
+            "username": user_data["email"],  # Email used for login
+            "password": user_data["password"],  # Password used for login
+            "scope": "",  # Optional (empty value sent)
+            "client_id": "",  # Optional (empty value sent)
+            "client_secret": "",  # Optional (empty value sent)
+        }
+        response = requests.post(login_url, data=login_data)
+        if response.status_code == 200:
+            print("User logged in successfully!")
+            access_token = response.json().get("access_token")
+            if access_token:
+                print("Access token received.")
+                os.environ["AUTH_TOKEN"] = access_token
+                set_key(".env", "AUTH_TOKEN", access_token)
+
+                print("Access token saved to environment.")
+            else:
+                print("No access token found in response.")
+    else:
+        print("Failed to register user:", response.json())
+
+    return response.json()
+
+
+"""
+===================================================
+Authentication functions
+===================================================
+"""
+### POST /signup
+## TODO: now assume user has already signed up using UI
+
+
+### POST /login
+## TODO: now assume user has already logged in using UI
+def login_with_email_password_and_set_access_token(
+    email: str, password: str, verbose: bool = False
+):
+    load_dotenv(override=True)
+    BACKEND_DB_URL = os.getenv("BACKEND_DB_URL")
+    CLIENT_ID = os.getenv("CLIENT_ID", None)
+    CLIENT_SECRET = os.getenv("CLIENT_SECRET", None)
+    SCOPE = os.getenv("AUTH_SCOPE", None)
+
+    payload = {
+        "grant_type": "password",
+        "username": email,
+        "password": password,
+        "scope": SCOPE,
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+    }
+
+    response = requests.post(f"{BACKEND_DB_URL}/login", data=payload)
+
+    if response.status_code == 200:
+        token_data = response.json()
+        if verbose:
+            print("✅ Login successful. Access token received.")
+        os.environ["AUTH_TOKEN"] = token_data.get("access_token")
+        return token_data
+    else:
+        if verbose:
+            print("❌ Login failed:", response.status_code)
+        return response.json()
+
+
+"""
+===================================================
+Booking functions
+===================================================
+"""
+
+
+### GET /bookings/available_slots
+# remark: assumed logged in
+# return: list of {id (booking_id), datetime, polyclinic, vaccine_id}
+def get_available_booking_slots(
+    vaccine_name: str,
+    polyclinic_limit: int = None,
+    timeslot_limit: int = None,
+    verbose: bool = False,
+):
+    load_dotenv(override=True)
+    BACKEND_DB_URL = os.getenv("BACKEND_DB_URL")
+    auth_token = os.getenv("AUTH_TOKEN")
+
+    if not auth_token:
+        if verbose:
+            print("Access token not found.")
+        return
+
+    params = {"vaccine_name": vaccine_name}
+    if polyclinic_limit:
+        params["polyclinic_limit"] = polyclinic_limit
+    if timeslot_limit:
+        params["timeslot_limit"] = timeslot_limit
+
+    headers = {"Authorization": f"Bearer {auth_token}"}
+
+    response = requests.get(
+        f"{BACKEND_DB_URL}/bookings/available", params=params, headers=headers
+    )
+
+    if response.status_code == 200:
+        if verbose:
+            print("Available slots retrieved successfully.")
+        return response.json()
+    else:
+        if verbose:
+            print("Failed to fetch available slots:", response.status_code)
+        return response.json()
+
+
+### GET /bookings/{id}
+# description: get booking details by booking_id
+def get_booking_by_id(booking_id: str, verbose: bool = False):
+    load_dotenv(override=True)
+    BACKEND_DB_URL = os.getenv("BACKEND_DB_URL")
+    auth_token = os.getenv("AUTH_TOKEN")
+
+    if not auth_token:
+        if verbose:
+            print("Access token not found.")
+        return
+
+    headers = {"Authorization": f"Bearer {auth_token}"}
+
+    response = requests.get(f"{BACKEND_DB_URL}/bookings/{booking_id}", headers=headers)
+
+    if response.status_code == 200:
+        if verbose:
+            print("Booking details retrieved.")
+        return response.json()
+    else:
+        if verbose:
+            print("Failed to get booking:", response.status_code)
+        return response.json()
+
+
+### POST /bookings/schedule
+# remark: booking_slot_id can be obtained from get_available_booking_slots["slot_index"]["id"]
+def schedule_vaccination_slot(booking_slot_id: str, verbose: bool = False):
+    load_dotenv(override=True)
+    BACKEND_DB_URL = os.getenv("BACKEND_DB_URL")
+    auth_token = os.getenv("AUTH_TOKEN")
+
+    if not auth_token:
+        if verbose:
+            print("Access token not found.")
+        return
+
+    headers = {
+        "Authorization": f"Bearer {auth_token}",
+        "Content-Type": "application/json",
+    }
+
+    payload = {"booking_slot_id": booking_slot_id}
+
+    response = requests.post(
+        f"{BACKEND_DB_URL}/bookings/schedule", json=payload, headers=headers
+    )
+
+    if response.status_code == 201:
+        if verbose:
+            print("Vaccination slot scheduled successfully.")
+        return response.json()
+    else:
+        if verbose:
+            print("Failed to schedule slot:", response.status_code)
+        return response.json()
+
+
+### DELETE /bookings/cancel/{record_id}
+# remark: assumed logged in
+# description: cancel a booking by record_id, i.e. primary key in vaccine_records table
+def cancel_booking(record_id: str, verbose: bool = False):
+    load_dotenv(override=True)
+    BACKEND_DB_URL = os.getenv("BACKEND_DB_URL")
+    auth_token = os.getenv("AUTH_TOKEN")
+
+    if not auth_token:
+        if verbose:
+            print("Access token not found.")
+        return
+
+    headers = {"Authorization": f"Bearer {auth_token}"}
+
+    response = requests.delete(
+        f"{BACKEND_DB_URL}/bookings/cancel/{record_id}", headers=headers
+    )
+
+    if response.status_code == 200:
+        if verbose:
+            print("Booking cancelled successfully.")
+        return response.json()
+    else:
+        if verbose:
+            print("Failed to cancel booking:", response.status_code)
+        return response.json()
+
+
+"""
+===================================================
+Chat functions
+===================================================
+"""
+### POST /chat
+
+"""
+===================================================
+Record functions (vaccination)
+===================================================
+"""
+
+
+### GET /records
+# login needed, i.e. must have auth_token in environment
+# description: get vaccination history for currently login user
+def get_vaccination_history(verbose: bool = False):
+    load_dotenv(override=True)
+    BACKEND_DB_URL = os.getenv("BACKEND_DB_URL")
+    auth_token = os.getenv("AUTH_TOKEN")
+
+    if not auth_token:
+        if verbose:
+            print("Access token not found. Please register and log in first.")
+        return
+
+    # Define the URL for fetching vaccination records
+    records_url = f"{BACKEND_DB_URL}/records"
+
+    # Set headers with the Authorization token
+    headers = {
+        "Authorization": f"Bearer {auth_token}",
+        "Accept": "application/json",
+    }
+
+    # Send the GET request
+    response = requests.get(records_url, headers=headers)
+    if response.status_code == 200:
+        if verbose:
+            print("Vaccination history retrieved successfully!")
+        return response.json()
+    elif (
+        response.status_code == 404
+        and response.json().get("detail") == "No records found."
+    ):
+        if verbose:
+            print("No vaccination records found.")
+        return response.json()
+    else:
+        if verbose:
+            print(
+                f"Failed to retrieve vaccination history. Status code: {response.status_code}"
+            )
+            print(response.json())
+
+
+"""
+===================================================
+User functions
+===================================================
+"""
+
+
+### GET /users
+# remark: login needed, i.e. must have auth_token in environment
+# description: get user details of the currently logged in user
+def get_user_details(verbose: bool = False):
+    load_dotenv(override=True)
+    BACKEND_DB_URL = os.getenv("BACKEND_DB_URL")
+    auth_token = os.getenv("AUTH_TOKEN")
+
+    if not auth_token and verbose:
+        print("Access token not found. Please register and log in first.")
+        return
+
+    # Define the URL for fetching user details
+    user_details_url = f"{BACKEND_DB_URL}/users/"
+
+    # Set headers with the Authorization token
+    headers = {
+        "Authorization": f"Bearer {auth_token}",
+        "Content-Type": "application/json",
+    }
+
+    # Send the POST request
+    response = requests.get(user_details_url, headers=headers)
+
+    if response.status_code == 200:
+        if verbose:
+            print("User details retrieved successfully!")
+        user_details = response.json()
+        # print(user_details)
+        return user_details
+    else:
+        if verbose:
+            print(
+                f"Failed to retrieve user details. Status code: {response.status_code}"
+            )
+        return response.json()
+
+
+### PUT /users
+### DELETE /users/{id}
+
+
+"""
+===================================================
+Vaccine functions
+===================================================
+"""
+
+### GET /vaccines/recommendations
+
+"""
+===================================================
+default functions
+===================================================
+"""
+
+
+# Test the functions
+if __name__ == "__main__":
+
+    pass
---
+++ notebooks/autogen/agent_tools_wrapper.py
@@ -0,0 +1,149 @@
+"""
+This files contains the wrapper for the agent_tools module.
+Modifiable part: descriptions of functions when wrapped with autogen_tools
+"""
+
+from agent_tools import (
+    cancel_booking,
+    get_available_booking_slots,
+    get_booking_by_id,
+    get_user_details,
+    get_vaccination_history,
+    login_with_email_password_and_set_access_token,
+    schedule_vaccination_slot,
+)
+from autogen_core.tools import FunctionTool
+
+# from agent_tools_wrapper import (
+#     get_available_booking_slots_tool,
+#     get_booking_by_id_tool,
+#     schedule_vaccination_slot_tool,
+#     cancel_booking_tool,
+#     get_vaccination_history_tool,
+#     get_user_details_tool
+# )
+"""
+===================================================
+Development functions
+===================================================
+"""
+
+
+"""
+===================================================
+Authentication functions
+===================================================
+"""
+### POST /signup
+## TODO: now assume user has already signed up using UI
+
+### POST /login
+login_with_email_password_and_set_access_token_tool = FunctionTool(
+    login_with_email_password_and_set_access_token,
+    description="Logs in a user with an email and password, and sets the access token to environment so all functions are authenticated to be used.",
+)
+
+"""
+===================================================
+Booking functions
+===================================================
+"""
+
+### GET /bookings/available_slots
+# remark: assumed logged in
+# return: list of {id (booking_id), datetime, polyclinic, vaccine_id}
+get_available_booking_slots_tool = FunctionTool(
+    get_available_booking_slots,
+    description="Returns available booking slots for a specified vaccine."
+    "Accepts optional polyclinic and timeslot limits, which filters the list."
+    "The response is a list, each entry includes booking ID('id'), datetime, polyclinic, and vaccine_id.",
+)
+
+
+### GET /bookings/{id}
+# description: get booking details by booking_id
+get_booking_by_id_tool = FunctionTool(
+    get_booking_by_id,
+    description="Retrieves booking details for a specific booking slot using its ID, regardless of the status being booked, cancelled, or completed."
+    "The details include the booking ID ('id'), datetime, polyclinic, and vaccine_id.",
+)
+
+### POST /bookings/schedule
+# remark: booking_slot_id can be obtained from get_available_booking_slots["slot_index"]["id"]
+schedule_vaccination_slot_tool = FunctionTool(
+    schedule_vaccination_slot,
+    description="Schedules a vaccination slot for the logged-in user by providing a valid booking slot id (the 'id' from booking slot)."
+    "If successful, the response includes the vaccine record ID ('id'), which has a field with values 'booked' or 'completed'.",
+)
+
+### DELETE /bookings/cancel/{record_id}
+# remark: assumed logged in
+# description: cancel a booking by record_id, i.e. primary key in vaccine_records table
+cancel_booking_tool = FunctionTool(
+    cancel_booking,
+    description="Cancels an existing vaccination appointment by specifying the record ID.",
+)
+
+
+"""
+===================================================
+Chat functions
+===================================================
+"""
+### POST /chat
+
+"""
+===================================================
+Record functions (vaccination)
+===================================================
+"""
+### GET /records
+# login needed, i.e. must have auth_token in environment
+# description: get vaccination history for currently login user
+get_vaccination_history_tool = FunctionTool(
+    get_vaccination_history,
+    description="Fetch the vaccination history of the currently logged-in user."
+    "Each vaccination record includes a unique record ID ('id'), unique booking_slot_id from the booking slots table, time created_at, and status ('booked' or 'completed').",
+)
+
+
+"""
+===================================================
+User functions
+===================================================
+"""
+### GET /users
+# remark: login needed, i.e. must have auth_token in environment
+# description: get user details of the currently logged in user
+get_user_details_tool = FunctionTool(
+    get_user_details,
+    description=(
+        "Retrieves the currently logged-in user's details, including NRIC, first name, "
+        "last name, email, date of birth, gender, address, enrolled clinic, and timestamps "
+        "for account creation and last update."
+    ),
+)
+
+
+### PUT /users
+### DELETE /users/{id}
+
+
+"""
+===================================================
+Vaccine functions
+===================================================
+"""
+
+### GET /vaccines/recommendations
+
+"""
+===================================================
+default functions
+===================================================
+"""
+
+
+if __name__ == "__main__":
+    # test the functions
+    print("Agent tools wrapper loaded.")
