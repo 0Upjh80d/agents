8263c010b7e7b7d28a324fa848d71061c2dfd5e1
Lyndon Lim <hnghng127@gmail.com>
2025-04-02 14:12:56 +0800

    refactor: combine multiple test permutations into one

--- app/backend/app/tests/test_clinic.py
+++ app/backend/app/tests/test_clinic.py
@@ -5,52 +5,35 @@ from schemas.clinic import ClinicResponse, ClinicType
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("clinic_limit", [1, 2])
-async def test_authorized_user_get_nearest_polyclinic(
+@pytest.mark.parametrize(
+    "clinic_type, clinic_limit",
+    [("polyclinic", 1), ("polyclinic", 2), ("gp", 1), ("gp", 2), (None, 1), (None, 2)],
+)
+async def test_authorized_user_get_nearest(
     authorized_client: AsyncClient,
-    clinic_limit: int,
-):
-    params = {"clinic_type": "polyclinic", "clinic_limit": clinic_limit}
-    res: Response = await authorized_client.get("/clinic/nearest", params=params)
-
-    assert res.status_code == 200
-    assert len(res.json()) == clinic_limit
-
-    polyclinics = [ClinicResponse(**record) for record in res.json()]
-
-    for polyclinic in polyclinics:
-        assert polyclinic.type == ClinicType.POLYCLINIC
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize("clinic_limit", [1, 2])
-async def test_authorized_user_get_nearest_gp(
-    authorized_client: AsyncClient,
-    clinic_limit: int,
-):
-    params = {"clinic_type": "gp", "clinic_limit": clinic_limit}
-    res: Response = await authorized_client.get("/clinic/nearest", params=params)
-
-    assert res.status_code == 200
-    assert len(res.json()) == clinic_limit
-
-    gps = [ClinicResponse(**record) for record in res.json()]
-
-    for gp in gps:
-        assert gp.type == ClinicType.GENERAL_PRACTIONER
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize("clinic_limit", [1, 2])
-async def test_authorized_user_get_nearest_clinic(
-    authorized_client: AsyncClient,
+    clinic_type: str,
     clinic_limit: int,
 ):
     params = {"clinic_limit": clinic_limit}
+
+    if clinic_type:
+        params["clinic_type"] = clinic_type
+
     res: Response = await authorized_client.get("/clinic/nearest", params=params)
 
     assert res.status_code == 200
     assert len(res.json()) == clinic_limit
+
+    clinics = [ClinicResponse(**record) for record in res.json()]
+
+    for clinic in clinics:
+        if clinic_type == ClinicType.POLYCLINIC.value:  # Get nearest polyclinic
+            assert clinic.type == ClinicType.POLYCLINIC
+        elif clinic_type == ClinicType.GENERAL_PRACTIONER.value:  # Get nearest GP
+            assert clinic.type == ClinicType.GENERAL_PRACTIONER
+        else:
+            # If none, check that the type is either POLYCLINIC or GENERAL_PRACTIONER only
+            assert clinic.type in {ClinicType.POLYCLINIC, ClinicType.GENERAL_PRACTIONER}
 
 
 @pytest.mark.asyncio
